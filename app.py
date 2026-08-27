1d1f2b8b6dd34fad58cca34380ce126181b2dc9d
meghavathshailaja <meghavathshailaja@gmail.com>
2026-08-27 15:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('registration.html')
+    return render_template('register.html')
 
 @app.route('/register', methods=['POST'])
 def register():
